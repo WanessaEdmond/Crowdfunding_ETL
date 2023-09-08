64eeb0639cb0850493dffd76a47ae92a663a791e
sladertorres <134868789+sladertorres@users.noreply.github.com>
2023-09-08 19:26:33 -0400

    Update crowdfunding_db.sql

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -1,11 +1,11 @@
 CREATE TABLE category (
-	category_id varchar PRIMARY KEY,
+	category_ids varchar PRIMARY KEY,
 	category varchar
 
 );
 
 CREATE TABLE subcategory (
-	subcategory_id varchar PRIMARY KEY,
+	subcategory_ids varchar PRIMARY KEY,
 	subcategory varchar
 
 );
